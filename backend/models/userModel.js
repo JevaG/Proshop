75941030c41b32456638c4ecd0e72371bfe8fd87
Jevria <jevriya.guliyeva@gmail.com>
2020-11-03 18:44:03 +0000

    Auth

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import  mongoose from  'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = mongoose.Schema (
     {
@@ -27,8 +28,22 @@ const userSchema = mongoose.Schema (
         when I have created document) properties on your document
     */
         timestamps: true
-})
+});
 
-const User = mongoose.model('User', userSchema)
 
-export default User
+userSchema.methods.matchPassword = async  function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await  bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,39 +1,13 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
 const router = express.Router();
-import Product from '../models/productModel.js';
+import {
+    getProducts,
+    getProductById
+} from '../controllers/productController.js';
 
 
-// @desc    fetch all products
-// @route   get/api/products
-// @access  Public
-router.get(
-    '/',
-    asyncHandler(async (req,res) => {
-        const products = await Product.find({})
+router.route('/').get(getProducts);
 
-        res.json(products)
-    })
-)
+router.route('/:id').get(getProductById);
 
-
-// @desc    fetch single product
-// @route   get/api/products/:id
-// @access  Public
-router.get(
-    '/:id',
-    asyncHandler( async (req,res) => {
-        const product = await Product.findById(req.params.id)
-
-        if (product){
-            res.json(product)
-        } else {
-            res.status(404)
-            throw new Error('Product Not Found')
-        }
-
-    })
-)
-
-
-export default router
+export default router;
