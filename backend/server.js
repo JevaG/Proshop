2bba052668dbcad890722e7388d77419c0a802c6
Jevria <jevriya.guliyeva@gmail.com>
2020-11-30 20:00:35 +0000

    Changed default port

--- backend/server.js
+++ backend/server.js
@@ -55,7 +55,8 @@ if (process.env.NODE_ENV === 'production') {
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT;
 
 app.listen(
     PORT,
