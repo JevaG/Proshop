bcacbbb21e5f0eb1ed97937c660f993ebbb3811c
Jevria <jevriya.guliyeva@gmail.com>
2020-11-30 19:22:34 +0000

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -37,20 +37,20 @@ app.use('/api/config/paypal', (req, res) =>
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-if(process.env.NODE_ENV === 'production') {
-    if (process.env.NODE_ENV === 'production') {
-        app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-        app.get('*', (req, res) =>
-            res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-        )
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-    } else {
-        app.get('/', (req, res) => {
-            res.send('Api is running ...')
-        });
-    }
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    )
+
+} else {
+    app.get('/', (req, res) => {
+        res.send('Api is running ...')
+    });
 }
+
 
 app.use(notFound);
 app.use(errorHandler);
